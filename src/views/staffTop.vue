9b0420769a530c8fd36d0340821e841b784147cd
*fQD1sd97jX <cookie.729@icloud.com>
2024-06-29 01:56:32 +0900

    feat: list

--- src/views/staffTop.vue
+++ src/views/staffTop.vue
@@ -3,13 +3,48 @@
     <h1>職員一覧</h1>
 
     <ul>
-      <li></li>
+      <template 
+        v-for="(staff, key) in staffList"
+        v-bind:key="key"
+      >
+        <li>
+          <p>
+            {{ staff }}
+          </p>
+        </li>
+      </template>
     </ul>
   </div>
 </template>
 
-
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const staffData: {
+  id: number;
+  code: string;
+  name: string;
+} =  {
+    id: 1,
+    code: '001',
+    name: 'test-name1'
+}
+  // {
+  //   id: 2,
+  //   code: '002',
+  //   name: 'test-name2'
+  // },
+  // {
+  //   id: 3,
+  //   code: '003',
+  //   name: 'test-name3'
+  // },
+  // {
+  //   id: 4,
+  //   code: '004',
+  //   name: 'test-name4'
+  // }
+
+
+const staffList = ref(staffData)
 </script>
