3f44d07d4d298d0884f4d9be20780b51e8e3cd6a
*fQD1sd97jX <cookie.729@icloud.com>
2024-07-06 01:33:11 +0900

    feat: v-model

--- src/views/addStaff.vue
+++ src/views/addStaff.vue
@@ -4,10 +4,29 @@
       <h1 class="content-title">職員追加</h1>
     </div>
     <form action="#" v-on:submit.prevent="onFormSubmit">
-      <input class="staff-form" type="text" required />
+      <div>
+        <label for="name">職員名</label>：
+        <input class="staff-form" type="text" required v-model="inputNameModel" />
+      </div>
+      <div>
+        <label for="name">部署</label>：
+        <input class="staff-form" type="text" required v-model="inputNameModel" />
+      </div>
+      <div>
+        <label for="name">役職</label>：
+        <input class="staff-form" type="text" required v-model="inputNameModel" />
+      </div>
+      <div>
+        <label for="name">入社日</label>：
+        <input class="staff-form" type="text" required v-model="inputNameModel" />
+      </div>
+
       <button type="submit">追加</button>
+      <p>{{ inputNameModel }}</p>
     </form>
     <p>{{ msg }}</p>
+
+    <button @click="test">テスト</button>
   </div>
 </template>
 
@@ -18,10 +37,20 @@ const msg = ref('未送信')
 const onFormSubmit = (): void => {
   msg.value = '送信されました'
 }
+
+const inputNameModel = ref(null)
+
+const test = () => {
+  console.log('確認', inputNameModel.value)
+}
 </script>
 
 <style lang="scss">
 .staff-form {
   background: rgb(238, 247, 255);
+}
+
+.label-width {
+  width: 500px;
 }
 </style>
